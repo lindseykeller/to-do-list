55e5e2e32525c33294a1537ed87fdc71b13145ab
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-04-13 16:36:53 -0700

    added functionality to do list app

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Todoform from "./Components/Todoform";
+import { listData } from "./Components/listData.js";
+import ToDoList from "./Components/ToDoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoData: listData,
+    };
+  }
+
+  toggleItem = itemNumber => {
+    this.setState({
+      todoData: this.state.todoData.map(item => {
+        if (itemNumber === item.number) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        }
+        return item;
+      })
+    });
+  };
+
+
+  addItem = (todoText) => {
+    const newTodo = {
+      name: todoText,
+      completed: false,
+      number: Date.now(),
+    };
+
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+  };
+
+  clearTask = (e) => {
+   
+    this.setState({
+      todoData: this.state.todoData.filter((item) => !item.completed),
+    });
+    console.log(this.state.todoData);
+  };
+
+  render() {
+    return (
+      <>
+    <div>     
+       <h1>To Do List</h1>
+           <Todoform addItem={this.addItem} />
     </div>
-  );
+       
+       
+       
+        <ToDoList
+         listData={this.state.todoData}
+          toggleItem={this.toggleItem}
+          clearTask={this.clearTask}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ to-do-app/src/Components/listData.js
@@ -0,0 +1,13 @@
+export const listData = [
+    {
+      name: "Go to store",
+      number: 1,
+      completed: false
+    },
+  
+    {
+      name: "study",
+      number: 2,
+      completed: false
+    }
+  ];
